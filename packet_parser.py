49eb70a71ff7307283989b0ae037fea72672a67a
Jeremy <jsp10@mac.com>
2020-11-13 15:19:06 -0500

    Basic reading functionality

--- packet_parser.py
+++ packet_parser.py
@@ -1,4 +1,18 @@
-def parse() :
-	print('called parse function in packet_parser.py')
+"""
+@Authors:
+	Jeremy Peters (Jsp7075)
+"""
 
-	
+def parse(filename):
+	file = open(filename, 'r')
+	raw = file.readlines()
+	for line in raw:
+		split = line.split(' ')
+		print(split)
+	file.close()
+
+def main():
+	parse("Captures/example.txt")
+
+if __name__ == '__main__':
+    main()
