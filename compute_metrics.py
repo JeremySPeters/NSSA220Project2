b4103098aa80a7b8f442b1cfc1b5971b70b35ac1
rjw9659 <rjw9659@rit.edu>
2020-11-17 19:26:46 -0500

    Added Comments

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,3 +1,12 @@
-def compute() :
+
+"""
+@Authors:
+	Jeremy Peters (Jsp7075)
+	Randall Weber (rjw9659)
+"""
+
+
+
+def compute():
 	print('called compute function in compute_metrics.py')
 
--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,6 +2,14 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
+"""
+@Authors:
+	Jeremy Peters (Jsp7075)
+	Randall Weber (rjw9659)
+"""
+
+
+
 filter()
 parse()
 compute()
