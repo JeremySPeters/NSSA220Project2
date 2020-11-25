b45b882688a0c1960f63b5a2abd63344cfb26909
Jeremy <jsp10@mac.com>
2020-11-24 20:40:49 -0500

    parse outputs to array

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,21 +1,31 @@
-from filter_packets import *
-from packet_parser import *
-from compute_metrics import *
-
 """
 @Authors:
-	Jeremy Peters (Jsp7075)
+	Jeremy Peters (jsp7075)
 	Randall Weber (rjw9659)
 	Julian Mato-Hernandez (jmm5458)
 """
 
+from filter_packets import *
+from packet_parser import *
+from compute_metrics import *
 
 def main():
-    inputNodes = ["Node1.txt" , "Node2.txt", "Node3.txt", "Node4.txt", "Node5.txt"]
-    outputNodes = ["Node1_filtered.txt" , "Node2_filtered.txt", "Node3_filtered.txt", "Node4_filtered.txt", "Node5_filtered.txt"]
+    inputNodes = ["Node1.txt",
+                  "Node2.txt",
+                  "Node3.txt",
+                  "Node4.txt",
+                  "Node5.txt"]
+
+    outputNodes = ["Node1_filtered.txt",
+                   "Node2_filtered.txt",
+                   "Node3_filtered.txt",
+                   "Node4_filtered.txt",
+                   "Node5_filtered.txt"]
+    headerNodes = []
+
     for i in range(len(inputNodes)):
         filter(inputNodes[i], 'ICMP', outputNodes[i])
-    parse()
+        headerNodes.append(parse(outputNodes[i]))
     compute()
 if __name__ == '__main__':
     main()
